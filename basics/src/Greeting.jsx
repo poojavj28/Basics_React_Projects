89e009755d43898ba2a97a68e194ee075089b26c
poojavj28 <poojavajramatti28@gmail.com>
2024-12-27 17:52:23 +0530

    props concept

---
+++ basics/src/Greeting.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Greeting(props) {
+    return (
+        <div>Hello {props.name}</div>
+    )
+}
---
+++ basics/src/Header.jsx
@@ -0,0 +1,12 @@
+
+export default function Header() {
+    return (
+        <div>
+            <h1>Some Component</h1>
+            <header>Some Header</header>
+        </div>
+      
+    );
+  }
+
+  
